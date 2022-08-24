72bc8989b4e97c9f25e1c68eed8731d0175c791d
“青菜白玉汤” <2075125282@qq.com>
2022-08-24 19:51:09 +0800

    :fire: 删除无用代码

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,31 +8,6 @@ import { isString, isObject } from "./is/index";
 function handleError(e: Error) {
   console.error(e);
 }
-
-/**
- * render 图标
- * */
-export function renderIcon(icon: Component) {
-  return () => h(NIcon, null, { default: () => h(icon) });
-}
-
-/**
- * render new Tag
- * */
-const newTagColors = { color: "#f90", textColor: "#fff", borderColor: "#f90" };
-export function renderNew(type = "warning", text = "New", color: object = newTagColors) {
-  return () =>
-    h(
-      NTag as any,
-      {
-        type,
-        round: true,
-        size: "small",
-        color,
-      },
-      { default: () => text },
-    );
-}
 /**
  *  深拷贝
  * */
