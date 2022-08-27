475e233a22b7871679fc2e5986d857e3c7953e8f
“青菜白玉汤” <2075125282@qq.com>
2022-08-27 18:11:43 +0800

    :pencil2: 更新项目描述信息

--- src/enums/webEnums.ts
+++ src/enums/webEnums.ts
@@ -1,8 +1,8 @@
 export enum ProjectEnum {
   // 项目名称
-  PROJECT_NAME = "soup-admin 中后台模板",
+  PROJECT_NAME = "silver-parakeet 一个简洁的在线编译器",
   // 版权信息
-  COPYRIGHT_INFORMATION = "MIT Licensed | Copyright © 2022-present 青菜白玉汤",
+  COPYRIGHT_INFORMATION = "MIT Licensed | Copyright © 2022-present soupLee",
   // 备案信息
   RECORD_INFORMATION = "粤备案xxx号",
 }
