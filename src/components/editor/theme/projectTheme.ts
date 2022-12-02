00c010a715720fa524ef10cf70397577a8938be1
“青菜白玉汤” <2075125282@qq.com>
2022-12-02 20:30:10 +0800

    :bug: 修复选中代码不高亮的问题

--- src/components/editor/theme/projectTheme.ts
+++ src/components/editor/theme/projectTheme.ts
@@ -1,45 +1,61 @@
 import { EditorView } from "codemirror";
 const bgColor = "#282c34";
-export const Theme = EditorView.theme({
-  "&": {
-    color: "#b0b7c3",
-    backgroundColor: bgColor,
+export const Theme = EditorView.theme(
+  {
+    "&": {
+      color: "#b0b7c3",
+      backgroundColor: bgColor,
+    },
+    ".cm-content": {
+      caretColor: bgColor,
+    },
+    // 光标
+    ".cm-cursor": {
+      borderLeftColor: "#fff",
+    },
+    // 选中的代码
+    "&.cm-focused .cm-selectionBackground, ::selection": {
+      backgroundColor: "#324464",
+      zIndex: "10",
+    },
+    // 光标所在行
+    ".cm-activeLine": {
+      backgroundColor: "rgba(47,50,59,0.4)",
+      // borderTop: "#2c2c2c 1px solid",
+      // borderBottom: "#2c2c2c 1px solid",
+    },
+    // 侧边行号栏
+    ".cm-gutters": {
+      backgroundColor: "#282c34",
+      color: "#535773",
+      border: "none",
+    },
+    // 选中的行号
+    ".cm-activeLineGutter": {
+      backgroundColor: "#282c34",
+      color: "#9DA5B3",
+    },
+    ".cm-line": {
+      fontFamily: "'JetBrainsMono'",
+    },
+    // "&": {
+    //   color: "white",
+    //   backgroundColor: "#034",
+    // },
+    // ".cm-content": {
+    //   caretColor: "#0e9",
+    // },
+    // "&.cm-focused .cm-cursor": {
+    //   borderLeftColor: "#0e9",
+    // },
+    // "&.cm-focused .cm-selectionBackground, ::selection": {
+    //   backgroundColor: "#074",
+    // },
+    // ".cm-gutters": {
+    //   backgroundColor: "#045",
+    //   color: "#ddd",
+    //   border: "none",
+    // },
   },
-  ".cm-content": {
-    caretColor: bgColor,
-  },
-  // 光标
-  ".cm-cursor": {
-    borderLeftColor: "#fff",
-  },
-  // 选中的代码
-  "&.cm-focused .cm-selectionBackground, ::selection": {
-    backgroundColor: "#394e75",
-  },
-  // 光标所在行
-  ".cm-activeLine": {
-    backgroundColor: "#2f323b",
-    // borderTop: "#2c2c2c 1px solid",
-    // borderBottom: "#2c2c2c 1px solid",
-  },
-  // 侧边行号栏
-  ".cm-gutters": {
-    backgroundColor: "#282c34",
-    color: "#535773",
-    border: "none",
-  },
-  // ".cm-gutterElement span": {
-  //   display: "none !important",
-  // },
-  // 选中的行号
-  ".cm-activeLineGutter": {
-    backgroundColor: "#282c34",
-    color: "#9DA5B3",
-  },
-  ".cm-line": {
-    fontFamily: "'JetBrainsMono'",
-  },
-  // ".ͼc": {
-  //   color: "#c678dd",
-  // },
-});
+  { dark: true },
+);
